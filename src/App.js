d1e8fea425dc1228f6d3fdcc0b4e61486935710a
Taylor Griffin <tmgriffin73@gmail.com>
2022-06-29 15:49:46 -0400

    complete step 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const handleChange = (e) => {
+    e.target.id === 'name'
+      ? setNewName(e.target.value)
+      : setNewNumber(e.target.value);
+    console.log(e.target);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    const listOfPeople = persons.map((person) => person.name);
+    if (listOfPeople.includes(newName)) {
+      window.alert(`${newName} already exists in the phonebook`);
+      return;
+    }
+
     setPersons([
       ...persons,
       {
         name: newName,
+        number: newNumber,
       },
     ]);
+    setNewName('');
+    setNewNumber('');
   };
 
   return (
@@ -18,8 +34,18 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name:
-          <input onChange={(e) => setNewName(e.target.value)} />
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            onChange={handleChange}
+            value={newName}
+          />
+          <div>
+            <label htmlFor="number">Number:</label>
+
+            <input type="text" onChange={handleChange} value={newNumber} />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -28,7 +54,9 @@ const App = () => {
       <h2>Numbers</h2>
 
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
