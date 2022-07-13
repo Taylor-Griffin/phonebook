5899114ed46b0ed9eb2d07ea25e403ccc6d61ffa
Taylor Griffin <tmgriffin73@gmail.com>
2022-07-13 09:28:14 -0400

    style change

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>My Phonebook</h1>
       <Notification message={message} errorStyle={errorStyle} />
       <Filter setFilteredName={setFilteredName} />
 
